3ef59df724d55b742ed5896bb0a9664c7e18a574
Chris Keating <christopherkeating@gmail.com>
2010-07-26 12:58:01 -0500

    updated caveat

--- Library/Formula/sshfs-fuse.rb
+++ Library/Formula/sshfs-fuse.rb
@@ -5,6 +5,12 @@ class SshfsFuse <Formula
   homepage 'http://fuse.sourceforge.net/sshfs.html'
   md5 '26e9206eb5169e87e6f95f54bc005a4f'
 
+  def caveats
+    <<-EOS.undent
+    This depends on the MacFUSE installation from http://code.google.com/p/macfuse/
+    MacFUSE must be installed prior to installing this formula.
+    EOS
+  end
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
